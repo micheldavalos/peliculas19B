07858e43a46a8ae0c5df7b63988b9c61c9918012
Michel Dávalos <michel.davalos@academicos.udg.mx>
2019-11-07 16:56:38 -0600

    Muestra las películas en el HomePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,22 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  movies:any;
+  constructor(public navCtrl: NavController,
+              public http: HttpClient) {
+    this.http.get('/peliculas/peliculas/')
+      .subscribe(data => {
+        this.movies = data;
+        console.log(JSON.stringify(data));
+      }, error => {
+        console.log(JSON.stringify(error));
+      });
   }
 
 }
